6c5c44cce5afde793ce81c14c49f6eaf615c42fd
Fellaine <max.goncharevych@gmail.com>
2023-05-28 08:43:50 +0300

    Code refactoring

--- scrape.py
+++ scrape.py
@@ -22,12 +22,10 @@ def get_team_urls() -> dict[str, str]:
     teams = dict(
         re.findall('www\.espn\.com/nba/team/_/name/(\w+)/(.+?)",', data)  # noqa W605
     )
-    # "bos": "boston-celtics", "bkn": "brooklyn-nets",
-    roster_urls = []
-    for key in teams.keys():
-        roster_urls.append(
-            "https://www.espn.com/nba/team/roster/_/name/" + key + "/" + teams[key]
-        )
+    roster_urls = [
+        f"https://www.espn.com/nba/team/roster/_/name/{key}/{value}"
+        for key, value in teams.items()
+    ]
     return dict(zip(teams.values(), roster_urls))
 
 
@@ -39,8 +37,7 @@ def get_players_of_team(roster_url: str) -> list[str]:
     data = f.read().decode("utf-8")
     sleep(0.3)
     pattern = r'href="https://www\.espn\.com/nba/player/_/id/\d+/[a-z-]+">([^<]*)</a>'
-    players = re.findall(pattern, data)
-    return players
+    return re.findall(pattern, data)
 
 
 def get_list_of_all_players() -> list[str]:
@@ -54,17 +51,17 @@ def get_list_of_all_players() -> list[str]:
         has_expired = datetime.now() - last_modified > timedelta(days=1)
     if not has_expired:
         with open("list_of_players.pickle", "rb") as fp:
-            all_list = pickle.load(fp)
+            all_players = pickle.load(fp)
     else:
         rosters = get_team_urls()
         # all_players = dict()
-        all_list = []
+        all_players = []
         # i = 0
         for team in rosters.keys():
             # if i >= 3:
             #     break
             print("Gathering player info for team: " + team)
-            all_list.extend(get_players_of_team(rosters[team]))
+            all_players.extend(get_players_of_team(rosters[team]))
             # all_players[team] = get_players_of_team(rosters[team])
             # i = i + 1
 
@@ -72,14 +69,14 @@ def get_list_of_all_players() -> list[str]:
         # for value in all_players.values():
         #     all_list.extend(value)
 
-        all_list = [i.replace("&#x27;", "'") for i in all_list]
+        all_players = [i.replace("&#x27;", "'") for i in all_players]
 
         with open("list_of_players.pickle", "wb") as fp:
-            pickle.dump(all_list, fp)
-    return all_list
+            pickle.dump(all_players, fp)
+    return all_players
 
 
-all_list = get_list_of_all_players()
+all_players = get_list_of_all_players()
 
 app = Flask(__name__)
 
@@ -97,21 +94,20 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/", methods=["GET", "POST"])
 def reverse_string():
-    if "user_list" in session.keys():
-        user_list = session["user_list"]
-    else:
-        session["user_list"] = all_list[:]
-        user_list = session["user_list"]
+    if "user_players" not in session.keys():
+        session["user_players"] = all_players[:]
+    user_players = session["user_players"]
     if request.method == "POST":
-        if not user_list:
-            outp = f"GG, you got all {len(all_list)} of them "
+        if not user_players:
+            outp = f"GG, you got all {len(all_players)} of them "
         else:
             user_input = request.form["user_input"]
-            matches = difflib.get_close_matches(user_input, user_list, n=1, cutoff=0.8)
-            if matches:
-                user_list.remove(matches[0])
-                session["user_list"] = user_list
-                outp = f"{len(all_list) - len(user_list)}/{len(all_list)}"
+            if matches := difflib.get_close_matches(
+                user_input, user_players, n=1, cutoff=0.8
+            ):
+                user_players.remove(matches[0])
+                session["user_list"] = user_players
+                outp = f"{len(all_players) - len(user_players)}/{len(all_players)}"
             else:
                 outp = "No such player found."
         return jsonify({"outp": outp})
